0d80839b137ff72fa96ae7dedb1462bf6c5f0136
dlabey <dlabey@fastmail.fm>
2014-06-24 18:29:32 -0700

    Adding values directory. resolves #1

--- build/tasks/contrib-concat.js
+++ build/tasks/contrib-concat.js
@@ -2,14 +2,17 @@ module.exports = function(grunt) {
     grunt.config.set('concat', {
         dev: {
             src: [
+                'build/parts/intro.js',
                 'src/bootstrap.js',
                 'src/constants/*.js',
                 'src/constants/config/dev.js',
+                'src/values/**/*.js',
                 'src/models/**/*.js',
                 'src/services/**/*.js',
                 'src/controllers/**/*.js',
                 'src/directives/**/*.js',
-                'src/filters/**/*.js'
+                'src/filters/**/*.js',
+                'build/parts/outro.js'
             ],
             dest: 'build/dist/app.js'
         },
@@ -26,8 +29,20 @@ module.exports = function(grunt) {
             dest: 'public/styles.css'
         },
         prod: {
-            src: ['<%= concat.dev.src %>'],
-            dest: '<%= concat.dev.dest %>'
+            src: [
+                'build/parts/intro.js',
+                'src/bootstrap.js',
+                'src/constants/*.js',
+                'src/constants/config/prod.js',
+                'src/values/**/*.js',
+                'src/models/**/*.js',
+                'src/services/**/*.js',
+                'src/controllers/**/*.js',
+                'src/directives/**/*.js',
+                'src/filters/**/*.js',
+                'build/parts/outro.js'
+            ],
+            dest: 'build/dist/app.js'
         }
     });
 
